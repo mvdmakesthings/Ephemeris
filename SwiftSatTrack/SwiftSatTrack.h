9127764d2eed2a5f0ccdd6e7b58694088679f73f
Michael VanDyke <michaeljamesvandyke@gmail.com>
2020-04-06 17:21:06 -0400

    Initial Commit

---
+++ SwiftSatTrack/SwiftSatTrack.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSatTrack.h
+//  SwiftSatTrack
+//
+//  Created by Michael VanDyke on 4/6/20.
+//  Copyright © 2020 Michael VanDyke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftSatTrack.
+FOUNDATION_EXPORT double SwiftSatTrackVersionNumber;
+
+//! Project version string for SwiftSatTrack.
+FOUNDATION_EXPORT const unsigned char SwiftSatTrackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSatTrack/PublicHeader.h>
+
+
